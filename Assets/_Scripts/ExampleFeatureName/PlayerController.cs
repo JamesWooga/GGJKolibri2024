67fe3a06f5434353ca826468e78fd17efc1173a9
James Studd <james.studd@live.co.uk>
2024-01-26 23:22:51 +0100

    Adds TMP, some player movement

--- Assets/_Scripts/ExampleFeatureName/PlayerController.cs
+++ Assets/_Scripts/ExampleFeatureName/PlayerController.cs
@@ -1,22 +1,44 @@
 using UnityEngine;
+using UnityEngine.UI;
+using Vector2 = UnityEngine.Vector2;
 
 namespace ExampleFeatureName
 {
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private float _speed;
+        [SerializeField] private Rigidbody2D _rigidbody;
+        [SerializeField] private float _forceAmount;
+        [SerializeField] private ForceMode2D _forceMode;
+        [SerializeField] private Rigidbody2D _playerBody;
+        [SerializeField] private float _angleMultiplier;
+        [SerializeField] private Image _moveRightIndicator;
+        [SerializeField] private Image _moveLeftIndicator;
+        [SerializeField] private Color _pressedColor;
+        [SerializeField] private Color _notPressedColor;
         
         private void Update()
         {
-            if (Input.GetKey(KeyCode.A))
+            var isPressingLeft = Input.GetKey(KeyCode.A);
+            var isPressingRight = Input.GetKey(KeyCode.D);
+            
+            if (isPressingLeft)
             {
-                transform.Translate(Vector3.left * (_speed * Time.deltaTime));
+                _rigidbody.AddForce(Vector2.left * _forceAmount, _forceMode);
             }
             
-            if (Input.GetKey(KeyCode.D))
+            if (isPressingRight)
             {
-                transform.Translate(Vector3.right * (_speed * Time.deltaTime));
+                _rigidbody.AddForce(Vector2.right * _forceAmount, _forceMode);
             }
+
+            _moveLeftIndicator.color = isPressingLeft ? _pressedColor : _notPressedColor;
+            _moveRightIndicator.color = isPressingRight ? _pressedColor : _notPressedColor;
+        }
+
+        private void FixedUpdate()
+        {
+            var signedAngle = Vector2.SignedAngle(Vector2.down, _playerBody.position);
+            _rigidbody.AddForce(Vector2.right * (signedAngle * _angleMultiplier), _forceMode);
         }
     }
 }
