16035e42c1bfaee19485ca5868d30556fe1d77d1
James Studd <james.studd@live.co.uk>
2024-01-27 17:32:22 +0100

    Frog.

---
+++ Assets/_Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,16 @@
+﻿using Spine.Unity;
+using UnityEngine;
+
+namespace _Scripts.Player
+{
+    public class PlayerAnimator : MonoBehaviour
+    {
+        [SerializeField] private SkeletonAnimation _skeletonAnimation;
+        [SerializeField] private float _speed;
+
+        private void Start()
+        {
+            _skeletonAnimation.AnimationState.SetAnimation(0, "animation", true);
+        }
+    }
+}
