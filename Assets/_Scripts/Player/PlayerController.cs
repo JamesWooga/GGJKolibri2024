d8f53a73148d4d58f742cddbce79707e1f910599
James Studd <james.studd@live.co.uk>
2024-01-27 14:46:22 +0100

    Adds comments

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -6,29 +6,34 @@ namespace _Scripts.Player
 {
     public class PlayerController : MonoBehaviour
     {
-        [Header("Movement")]
+        [Header("Setup")]
         [SerializeField] private Rigidbody2D _rigidbody;
         [SerializeField] private Rigidbody2D _bodyRigidbody;
-        [SerializeField] private float _forceAmount;
         [SerializeField] private ForceMode2D _forceMode;
-        [SerializeField] private float _torqueAmount;
-        
-        [Header("Rotation")]
         [SerializeField] private Transform _rotationalAnchorPoint;
-        [SerializeField] private float _maxMagnitude;
-        [SerializeField] private float _leanForceAmount;
-        
-        [Header("Objects")] 
         [SerializeField] private PlayerCatchPoint _leftCatchPoint;
         [SerializeField] private PlayerCatchPoint _rightCatchPoint;
-        [SerializeField] private float _objectForcePerKg;
+        
+        [Header("Tweakable Values (hover for description)")]
+        
+        [Header("Directly From Input")]
+        [Tooltip("How much acceleration the wheel will have"), SerializeField] private float _wheelForceAmount;
+        [Tooltip("How much acceleration the body rotation will have"), SerializeField] private float _bodyTorqueAmount;
+        
+        [Header("Rotation")]
+        [Tooltip("The top speed for the wheel to be able to go"), SerializeField] private float _maxWheelMagnitude;
+        [Tooltip("How much the player leaning will affect the movement of the wheel"), SerializeField] private float _leanForceAmount;
+        [Tooltip("The top speed for the body to rotate at"), SerializeField] private float _maxBodyTorque;
+        
+        [Header("Objects")] 
+        [Tooltip("How much the objects on the catch points will affect the wheel moving"), SerializeField] private float _objectForcePerKg;
         
         private void FixedUpdate()
         {
             CalculateInput();
             ApplyLeanForce();
-            ClampVelocity();
             CalculateObjectWeights();
+            ClampVelocity();
         }
 
         private void OnTriggerEnter2D(Collider2D other)
@@ -46,24 +51,26 @@ namespace _Scripts.Player
             
             if (isPressingLeft)
             {
-                _bodyRigidbody.AddTorque(_torqueAmount);
-                _rigidbody.AddForce(Vector2.left * _forceAmount, _forceMode);
+                _bodyRigidbody.AddTorque(_bodyTorqueAmount);
+                _rigidbody.AddForce(Vector2.left * _wheelForceAmount, _forceMode);
             }
             
             if (isPressingRight)
             {
-                _bodyRigidbody.AddTorque(-_torqueAmount);
-                _rigidbody.AddForce(Vector2.right * _forceAmount, _forceMode);
+                _bodyRigidbody.AddTorque(-_bodyTorqueAmount);
+                _rigidbody.AddForce(Vector2.right * _wheelForceAmount, _forceMode);
             }
         }
 
         private void ClampVelocity()
         {
             var velocity = _rigidbody.velocity.normalized;
-            if (_rigidbody.velocity.magnitude > _maxMagnitude)
+            if (_rigidbody.velocity.magnitude > _maxWheelMagnitude)
             {
-                _rigidbody.velocity = velocity * _maxMagnitude;
+                _rigidbody.velocity = velocity * _maxWheelMagnitude;
             }
+
+            _bodyRigidbody.totalTorque = Mathf.Min(_maxBodyTorque, _bodyRigidbody.totalTorque);
         }
 
         private void ApplyLeanForce()
