2d1bb89f9c409304c0ce7028028cbc67908fcacc
James Studd <james.studd@live.co.uk>
2024-01-27 17:41:22 +0100

    adds animation

--- Assets/_Scripts/Player/PlayerAnimator.cs
+++ Assets/_Scripts/Player/PlayerAnimator.cs
@@ -6,11 +6,14 @@ namespace _Scripts.Player
     public class PlayerAnimator : MonoBehaviour
     {
         [SerializeField] private SkeletonAnimation _skeletonAnimation;
+        [SerializeField] private Rigidbody2D _rigidbody;
+        [SerializeField] private Transform _target;
         [SerializeField] private float _speed;
 
         private void Start()
         {
-            _skeletonAnimation.AnimationState.SetAnimation(0, "animation", true);
+            var anim = _skeletonAnimation.AnimationState.SetAnimation(0, "animation", true);
+            anim.TimeScale = _speed;
         }
     }
 }
