828bdf719c52f65995e66c0aa9b3fb888d07a9c0
Matheus Caetano <matheus.caetano@kolibrigames.com>
2024-01-27 02:20:14 +0100

    Updated tightrope for maximum smoothness

--- Assets/_Scripts/TightropeDisplayController.cs
+++ Assets/_Scripts/TightropeDisplayController.cs
@@ -1,38 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class TightropeDisplayController : MonoBehaviour
 {
-    public LineRenderer tightrope;
+    private LineRenderer tightrope;
+    private EdgeCollider2D edgeCollider;
 
     public float tightropeAdjacentDistance = 0.4f;
 
     public Vector3 ropeInitialPosition = new Vector3(-0.5f, 2.48f, 1);
 
+    void Awake()
+    {
+      tightrope = GetComponent<LineRenderer>();
+      edgeCollider = GetComponent<EdgeCollider2D>();
+    }
+
     void OnCollisionStay2D(Collision2D other)
     {
        Vector3 collisionPoint = other.GetContact(0).point;
        Vector3 collisionOffset = new Vector3(tightropeAdjacentDistance, 0, 0);
-       tightrope.SetPosition(1, collisionPoint - collisionOffset);
-       tightrope.SetPosition(2, collisionPoint);
-       tightrope.SetPosition(3, collisionPoint + collisionOffset);
+       tightrope.SetPosition(1, collisionPoint - collisionOffset - (collisionOffset/2));
+       tightrope.SetPosition(2, collisionPoint - collisionOffset);
+       tightrope.SetPosition(3, collisionPoint);
+       tightrope.SetPosition(4, collisionPoint + collisionOffset);
+       tightrope.SetPosition(5, collisionPoint + collisionOffset + (collisionOffset/2));
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
        Vector3 collisionPoint = other.GetContact(0).point;
        Vector3 collisionOffset = new Vector3(tightropeAdjacentDistance, 0, 0);
-       tightrope.SetPosition(1, collisionPoint - collisionOffset);
-       tightrope.SetPosition(2, collisionPoint);
-       tightrope.SetPosition(3, collisionPoint + collisionOffset);
+       tightrope.SetPosition(1, collisionPoint - collisionOffset- (collisionOffset/2));
+       tightrope.SetPosition(2, collisionPoint- collisionOffset);
+       tightrope.SetPosition(3, collisionPoint);
+       tightrope.SetPosition(4, collisionPoint + collisionOffset);
+       tightrope.SetPosition(5, collisionPoint + collisionOffset + (collisionOffset/2));
     }
 
     void OnCollisionExit2D(Collision2D other)
     {
        Vector3 collisionOffset = new Vector3(tightropeAdjacentDistance, 0, 0);
-       tightrope.SetPosition(1, ropeInitialPosition - collisionOffset);
-       tightrope.SetPosition(2, ropeInitialPosition);
-       tightrope.SetPosition(3, ropeInitialPosition + collisionOffset);
+       tightrope.SetPosition(1, ropeInitialPosition - collisionOffset- (collisionOffset/2));
+       tightrope.SetPosition(2, ropeInitialPosition- collisionOffset);
+       tightrope.SetPosition(3, ropeInitialPosition);
+       tightrope.SetPosition(4, ropeInitialPosition + collisionOffset);
+       tightrope.SetPosition(5, ropeInitialPosition + collisionOffset +(collisionOffset/2));
     }
+
+
 }
