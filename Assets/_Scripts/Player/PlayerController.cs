fd40fe91d6a90745392d00f621486fba6edffb93
James Studd <james.studd@live.co.uk>
2024-01-27 01:29:29 +0100

    Updates player

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -6,18 +6,23 @@ namespace _Scripts.ExampleFeatureName
 {
     public class PlayerController : MonoBehaviour
     {
+        [Header("Movement")]
         [SerializeField] private Rigidbody2D _rigidbody;
         [SerializeField] private float _forceAmount;
         [SerializeField] private ForceMode2D _forceMode;
+        
+        [Header("Rotation")]
         [SerializeField] private Transform _rotationalAnchorPoint;
         [SerializeField] private float _maxMagnitude;
         [SerializeField] private float _maxRotate;
+        [SerializeField] private float _leanForceAmount;
         
         private void FixedUpdate()
         {
             CalculateInput();
             ClampVelocity();
             UpdateRotationalAnchor();
+            ApplyLeanForce();
         }
 
         private void CalculateInput()
@@ -52,7 +57,16 @@ namespace _Scripts.ExampleFeatureName
             var horizontalVelocity = _rigidbody.velocity.x;
             var targetAngle = horizontalVelocity.Remap(-_maxMagnitude, _maxMagnitude, _maxRotate, -_maxRotate);
 
-            _rotationalAnchorPoint.DORotate(new Vector3(0f, 0f, targetAngle), 0.01f, RotateMode.LocalAxisAdd);
+            _rotationalAnchorPoint.DORotate(new Vector3(0f, 0f, targetAngle), 0.01f, RotateMode.LocalAxisAdd)
+                .SetLink(gameObject, LinkBehaviour.KillOnDestroy);
+        }
+
+        private void ApplyLeanForce()
+        {
+            var angle = _rotationalAnchorPoint.rotation.eulerAngles.z;
+            var updated = Mathf.Repeat(angle + 180, 360) - 180;
+            var lean = updated.Remap(-90f, 90f, -_leanForceAmount, _leanForceAmount);
+            _rigidbody.AddForce(new Vector2(-lean, 0f), _forceMode);
         }
     }
 }
---
+++ Assets/_Scripts/Scenes/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Scripts.Scenes
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.R))
+            {
+                var activeScene = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(activeScene.name);
+            }
+        }
+    }
+}
