f6a7674aafb7eccbcc157a2f570b6633645369f0
James Studd <james.studd@live.co.uk>
2024-01-28 14:20:38 +0100

    Adds delay when losing

--- Assets/_Scripts/Scenes/SceneLoader.cs
+++ Assets/_Scripts/Scenes/SceneLoader.cs
@@ -11,10 +11,15 @@ namespace _Scripts.Scenes
     public class SceneLoader : MonoBehaviour
     {
         private IDisposable _listener;
+        [SerializeField] private float _delay;
+
+        private float _time;
+        private bool _hasRestarted;
         
         private void OnEnable()
         {
             _listener = InputSystem.onAnyButtonPress.Call(TryRestart);
+            GameManager.Instance.OnGameStateUpdated += HandleGameStateUpdated;
         }
 
         private void OnDisable()
@@ -22,10 +27,19 @@ namespace _Scripts.Scenes
             _listener.Dispose();
         }
 
+        private void HandleGameStateUpdated(GameState.GameState obj)
+        {
+            if (obj == GameState.GameState.GameOver)
+            {
+                _time = Time.time + _delay;    
+            }
+        }
+
         private void TryRestart(InputControl button)
         {
-            if (GameManager.Instance.GameState == GameState.GameState.GameOver && !GameManager.Instance.IsInputBlocked)
+            if (!_hasRestarted && Time.time > _time && GameManager.Instance.GameState == GameState.GameState.GameOver && !GameManager.Instance.IsInputBlocked)
             {
+                _hasRestarted = true;
                 var activeScene = SceneManager.GetActiveScene();
                 MusicPlayer.Instance.Restart();
                 SceneManager.LoadScene(activeScene.name);
